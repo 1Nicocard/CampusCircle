e8495d83c2dbe30c9082d6eadfdb93047d1f286c
Hanna Muriel <hannamuriel2006@gmail.com>
2025-11-26 19:12:38 -0500

    Final commit

--- project/src/Pages/profile/Profile.tsx
+++ project/src/Pages/profile/Profile.tsx
@@ -29,7 +29,7 @@ export default function Profile() {
   const dispatch = useDispatch<AppDispatch>();
   const [posts, setPosts] = useState<Post[]>([]);
   const [profile, setProfile] = useState<any | null>(null);
-  const [, setLoading] = useState(false);
+
 
 
   // load profile and posts from Supabase when possible
